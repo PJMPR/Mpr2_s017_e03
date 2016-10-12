150126f15ce800b7207331e401f66d10a5ed702b
mati <mati@172.17.16.53>
2016-10-12 15:40:11 +0200

    Dodana fabryka.

--- src/main/java/domain/Money.java
+++ src/main/java/domain/Money.java
@@ -1,20 +1,27 @@
 package domain;
 
-public class Money {
+public abstract class Money {
 
     private int amount;
 
     public int getAmount() {
         return amount;
     }
-
     
     public Money(int amount) {
         this.amount = amount;
     }
 
+    public static Dollar dollar(int amount){
+        return new Dollar(amount);
+    }
     
-
+    public static Franc franc(int amount){
+        return new Franc(amount);
+    }
+   
+    public abstract Money times(int multiplier);
+    
     public boolean equals(Object other) {
         Money money = (Money) other;
         return money.amount == this.amount 
