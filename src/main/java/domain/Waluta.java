40ef9aa6d38a767a45ea81dbc6f336145b7c0214
student <student@virtualbox>
2016-10-12 15:00:06 +0200

    Franki s13900

---
+++ src/main/java/domain/Waluta.java
@@ -0,0 +1,20 @@
+package domain;
+
+public class Waluta {
+	
+	private int amount;
+	
+	public Waluta(int amount) {
+		this.amount = amount;
+	}
+	
+	public Waluta times(int multiplier) {
+		return new Waluta( amount * multiplier);
+	}
+	
+	public boolean equals(Object other){
+		Waluta waluta = (Waluta) other;
+		return waluta.amount==this.amount;
+	}
+
+}
