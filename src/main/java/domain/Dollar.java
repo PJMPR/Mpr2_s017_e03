5365fc20d829477ebea673a7223e83ae5adabf5e
mati <mati@172.17.16.53>
2016-10-12 14:27:29 +0200

    Przeciazenie konstruktora w Dollar. Override equals

--- src/main/java/domain/Dollar.java
+++ src/main/java/domain/Dollar.java
@@ -2,28 +2,25 @@ package domain;
 
 public class Dollar {
 
-	private int initialAmount;
-        public int amount;
+    private int initialAmount;
+    public int amount;
 
-	public int Dollar(int amount) {
-		this.initialAmount = amount;
-                return this.initialAmount;
-	}
-        
-        public Dollar(int amount) {
-		this.initialAmount = amount;
-	}
+    public int Dollar(int amount) {
+        this.initialAmount = amount;
+        return this.initialAmount;
+    }
+
+    public Dollar(int amount) {
+        this.initialAmount = amount;
+    }
 
     @Override
     public boolean equals(Object obj) {
-        return this.amount==((Dollar)obj).amount;
+        return this.amount == ((Dollar) obj).amount;
     }
-        
-        
-        
 
-	public Dollar times(int multiplier) {
-		amount = initialAmount * multiplier;
-		return this;
-	}
+    public Dollar times(int multiplier) {
+        amount = initialAmount * multiplier;
+        return this;
+    }
 }
--- src/test/java/domain/MoneyTest.java
+++ src/test/java/domain/MoneyTest.java
@@ -6,19 +6,17 @@ import junit.framework.TestCase;
 
 public class MoneyTest extends TestCase {
 
-	
-	@Test
-	public void testMultiplication(){
-		Dollar five = new Dollar(5);
-		Dollar product = five.times(2);
-		assertEquals(10, product.amount);
-		product = five.times(3);
-		assertEquals(15, product.amount);
-	} 
-        
-        
-        @Test
-	public void testEquality(){
-		assertEquals(new Dollar(5), new Dollar(5));
-	}
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
+    }
+
+    @Test
+    public void testEquality() {
+        assertEquals(new Dollar(5), new Dollar(5));
+    }
 }
