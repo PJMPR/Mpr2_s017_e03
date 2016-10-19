9f0dde5e1a3e30656c8dc32efbc1b3f0fe357ec3
mati <mati@172.17.16.17>
2016-10-19 15:42:28 +0200

    Dodana mapa walut

--- src/main/java/domain/Bank.java
+++ src/main/java/domain/Bank.java
@@ -1,9 +1,35 @@
 package domain;
 
+import java.util.HashMap;
+
 public class Bank {
 
-	public Money reduce(Expression expression, Currency currency) {
-		return expression.reduce(currency);
-	}
+    HashMap<Currency, HashMap<Currency, Integer>> rateMap;
+
+    public Bank() {
+        this.rateMap = new HashMap<Currency, HashMap<Currency, Integer>>();
+    }
+
+    public Boolean addRate(Currency baseCurrency, Currency toCurrency, Integer value) {
+        if (rateMap.get(baseCurrency) != null) {
+            rateMap.get(baseCurrency).put(toCurrency, value);
+            return true;
+        } else {
+            rateMap.put(baseCurrency, new HashMap<Currency, Integer>());
+            rateMap.get(baseCurrency).put(toCurrency, value);
+            return true;
+        }
+    }
+
+    public Money reduce(Expression expression, Currency currency) {
+        if (rateMap.containsKey(expression.reduce(currency).currency)
+                && rateMap.get(expression.reduce(currency).currency).containsKey(currency)) {
+            return new Money(
+                    ((Money) expression).getAmount()
+                    / rateMap.get(((Money) expression).currency).get(currency), currency);
+        } else {
+            return expression.reduce(currency);
+        }
+    }
 
 }
