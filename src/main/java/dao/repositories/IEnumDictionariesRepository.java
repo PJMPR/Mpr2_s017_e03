3aed20d44071654592787d7203b7322ff52c82e4
student <student@virtualbox>
2016-11-23 16:19:21 +0100

    change in EnumDictionaryRepository

--- src/main/java/dao/repositories/IEnumDictionariesRepository.java
+++ src/main/java/dao/repositories/IEnumDictionariesRepository.java
@@ -8,5 +8,5 @@ public interface IEnumDictionariesRepository extends IRepository<EnumDictionary>
 
 	public List<EnumDictionary> withDictionaryName(String enumName);
 	public List<EnumDictionary> getEnumValues(String enumName, String stringKey);
-	public List<EnumDictionary> getEnumValues(String enumName, int stringKey);
+	public List<EnumDictionary> getEnumValues(String enumName, int intKey);
 }
