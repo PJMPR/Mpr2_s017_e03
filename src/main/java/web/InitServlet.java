800f37431d420c6a1d379fc8262e8b2e3be4ac3f
Veok <p.lelental@gmail.com>
2016-12-21 14:48:07 +0100

    fix init

--- src/main/java/web/InitServlet.java
+++ src/main/java/web/InitServlet.java
@@ -19,7 +19,6 @@ public class InitServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
         TemporaryCurrencyEnumHelper temporaryCurrencyEnumHelper = new TemporaryCurrencyEnumHelper();
         temporaryCurrencyEnumHelper.insertCurrencyEnums();
     }
