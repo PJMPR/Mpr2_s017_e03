da5904fc91716abb560d8583234091cef11e536b
student <student@virtualbox>
2017-01-25 15:24:33 +0100

    fix

--- src/main/java/rest/HistoryResources.java
+++ src/main/java/rest/HistoryResources.java
@@ -1,12 +1,12 @@
 package rest;
 
 import domain.model.History;
-import domain.model.Person;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.*;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -15,15 +15,9 @@ import org.dozer.Mapper;
 
 import rest.dto.HistoryDto;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-/**
- * @author L on 18.01.2017.
- */
 @Path("wallet/{id}/history")
 @Stateless
 public class HistoryResources {
@@ -31,16 +25,17 @@ public class HistoryResources {
 	Mapper mapper = new DozerBeanMapper();
     @PersistenceContext
     EntityManager entityManager;
-
+    
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<HistoryDto> getAll(){
+    public Response getAll(){
     	List<HistoryDto> result = new ArrayList<HistoryDto>();
         System.out.println("zaczynam pobierac dane");
     	for(History h: entityManager.createNamedQuery("history.all",History.class).getResultList()){
         	result.add(mapper.map(h, HistoryDto.class));
         }
         System.out.println("koncze");
-        return result;
+        return Response.ok(new GenericEntity<List<HistoryDto>>(result){}).build();
     }
+    
 }
